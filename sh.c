8a19319354b9a7281c0af867d0884022eb313b97
RicardoDelsordo702570 <31805464+RicardoDelsordo702570@users.noreply.github.com>
2018-09-02 19:32:47 -0500

    Shutdown working

--- sh.c
+++ sh.c
@@ -11,18 +11,26 @@ int main(int argc, char const *argv[]) {
   /** Input of the user. */
   char cmd[80];
 
+  /** Index to kill all processes.*/
+  int i = 0;
+
 
   while (1) {
   	printf("Sh> ");
   	scanf("%s",cmd);
     /** If the input is 'exit', kill the current process. */
-    if (strcmp("exit", cmd) == 0) {
+    if (strcmp("exit", cmd) == 0)
+    {
       return -1;
-    }  else if (strcmp("shut", cmd) == 0) {
-        //kill(getpid(), SIGTERM);
-        //kill(getppid(), SIGTERM);
-    } else {
-
+    }
+    /**Shuts down all six processes*/
+    else if (strcmp("shut", cmd) == 0)
+    {
+        execl("/usr/bin/killall", "killall","./init","./getty", "./sh", (char*)NULL);
+    }
+    /**Executes specific command on console*/
+    else
+    {
       /**
       * Reference to fopen: http://pubs.opengroup.org/onlinepubs/009696699/functions/popen.html
       * checked on september 1, 2018.
